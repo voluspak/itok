20f46d3bc112009427a5d59f149cd17dbb936310
Ivan Telleria <ivantelleria96@gmail.com>
2023-04-30 17:42:42 +0200

    bottom nav icons finnished

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -1,28 +1,41 @@
 import Link from 'next/link'
 import React from 'react'
-import { HomeIcon } from './Icons'
+import { ChatIcon, FriendsIcon, HomeIcon, PlusIcon, UserIcon } from './Icons'
+
+const BOTTOM_NAV_ITEMS_STYLE = 'flex flex-col justify-center items-center'
 
 const BottomNav: React.FC = () => {
   return (
     <nav className='absolute bottom-0 left-0 right-0 h-12 py-2 border-t border-zinc-500 bg-black text-white'>
     <ul className='flex justify-evenly items-center'>
       <li>
-        <Link href={'/'} className='flex flex-col justify-center items-center'>
+        <Link href={'/'} className={BOTTOM_NAV_ITEMS_STYLE}>
           <HomeIcon />
           Home
         </Link>
       </li>
       <li>
-        <Link href={'/'}>Friends</Link>
+        <Link href={'/'} className={`${BOTTOM_NAV_ITEMS_STYLE} pt-0.5`}>
+          <FriendsIcon />
+          Friends
+          </Link>
       </li>
       <li>
-        <Link href={'/'}>+</Link>
+        <Link href={'/'} className={`${BOTTOM_NAV_ITEMS_STYLE} bg-white px-3 py-1 rounded-lg`}>
+          <PlusIcon />
+        </Link>
       </li>
       <li>
-        <Link href={'/'}>Inbox</Link>
+        <Link href={'/'} className={`${BOTTOM_NAV_ITEMS_STYLE}`}>
+          <ChatIcon />
+          Inbox
+        </Link>
       </li>
       <li>
-        <Link href={'/'}>Profile</Link>
+        <Link href={'/'} className={BOTTOM_NAV_ITEMS_STYLE}>
+          <UserIcon />
+          Profile
+        </Link>
       </li>
     </ul>
   </nav>
--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -55,3 +55,28 @@ export const ShareIcon: React.FC = () => (
   </svg>
 
 )
+
+export const FriendsIcon: React.FC = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
+  </svg>
+)
+
+export const PlusIcon: React.FC = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-black">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+  </svg>
+)
+
+export const ChatIcon: React.FC = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 01-.825-.242m9.345-8.334a2.126 2.126 0 00-.476-.095 48.64 48.64 0 00-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0011.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155" />
+  </svg>
+
+)
+
+export const UserIcon: React.FC = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+  </svg>
+)
