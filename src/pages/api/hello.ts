2edcbe25d80fc180d4e449126ac7801689d88301
Ivan Telleria <ivantelleria96@gmail.com>
2023-05-03 10:08:49 +0200

    FIX: hide API_KEY

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -5,24 +5,6 @@ interface Data {
   name: string
 }
 
-export default async function handler (req: NextApiRequest, res: NextApiResponse<Data>): Promise<void> {
-  const url = 'https://tiktok-all-in-one.p.rapidapi.com/feed?region=US&device_id=7523368224928586621'
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '0d322a12e7mshde52d1409671e01p107fd1jsn667675247fe8',
-      'X-RapidAPI-Host': 'tiktok-all-in-one.p.rapidapi.com'
-    }
-  }
+export default function handler (req: NextApiRequest, res: NextApiResponse<Data>): void {
 
-  try {
-    const response = await fetch(url, options)
-    if (!response.ok) {
-      throw new Error('Fetching error')
-    }
-    const result = await response.json()
-    res.status(200).send(result.aweme_list)
-  } catch (error) {
-    console.error(error)
-  }
 }
