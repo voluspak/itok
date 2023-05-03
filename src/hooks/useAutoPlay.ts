3a3e1bca60862aeee45acca22f13848dbe88e70c
Ivan Telleria <ivantelleria96@gmail.com>
2023-05-03 13:30:29 +0200

    FIX: IO now only plays videos on screen

--- src/hooks/useAutoPlay.ts
+++ src/hooks/useAutoPlay.ts
@@ -1,5 +1,31 @@
 import { useRef, useEffect } from 'react'
 
+type IOCallback = IntersectionObserverCallback
+
+const callback: IOCallback = (entries) => {
+  const [entry] = entries
+
+  const videoElement = entry.target as HTMLVideoElement
+
+  if (!entry.isIntersecting) {
+    videoElement.pause()
+    return
+  }
+
+  if (entry.isIntersecting) {
+    videoElement.play().catch(error => {
+      console.log('Hubo un error con el intersection observer')
+      console.log(error)
+    })
+  }
+}
+
+const IOOptions = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 1.0
+}
+
 export const useAutoPlay = (): React.LegacyRef<HTMLVideoElement> => {
   const videoRef = useRef<HTMLVideoElement>(null)
 
@@ -7,23 +33,7 @@ export const useAutoPlay = (): React.LegacyRef<HTMLVideoElement> => {
     const video = videoRef.current
     if (video === null || video === undefined) return
 
-    const observer = new IntersectionObserver((entries) => {
-      const [entry] = entries
-
-      const videoElement = entry.target as HTMLVideoElement
-
-      if (!entry.isIntersecting) {
-        videoElement.pause()
-        return
-      }
-
-      if (entry.isIntersecting) {
-        videoElement.play().catch(error => {
-          console.log('Hubo un error con el intersection observer')
-          console.log(error)
-        })
-      }
-    })
+    const observer = new IntersectionObserver(callback, IOOptions)
 
     observer.observe(video)
 
